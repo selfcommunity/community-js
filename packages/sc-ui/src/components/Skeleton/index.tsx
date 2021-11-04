dfa88deba6dab975e717982831836e0471472c0e
mirk8xr <mirk8xr@gmail.com>
2021-11-04 15:36:08 +0100

    Merge branch 'dev' into widgets
    # Conflicts:
    #	packages/sc-core/package.json
    #	packages/sc-core/src/components/provider/SCAuthProvider/index.tsx
    #	packages/sc-ui/src/components/PeopleSuggestion/index.tsx
    #	packages/sc-ui/src/components/User/index.tsx

--- packages/sc-ui/src/components/Skeleton/index.tsx
+++ packages/sc-ui/src/components/Skeleton/index.tsx
@@ -2,5 +2,6 @@ import PersonBoxSkeleton from './PersonBoxSkeleton';
 import PostBoxSkeleton from './PostBoxSkeleton';
 import UserBoxSkeleton from './UserBoxSkeleton';
 import InterestBoxSkeleton from './InterestBoxSkeleton';
+import PeopleSuggestionSkeleton from './PeopleSuggestionSkeleton';
 
-export {PersonBoxSkeleton, PostBoxSkeleton, UserBoxSkeleton, InterestBoxSkeleton};
+export {PersonBoxSkeleton, PostBoxSkeleton, UserBoxSkeleton, InterestBoxSkeleton, PeopleSuggestionSkeleton};
