1f38105b0c166e64e09d3f1be19dfcd2f8ce9c65
Federico Torresan <federico.torresan@quentral.com>
2021-12-21 15:29:42 +0100

    refactor feed component

--- packages/sc-ui/src/components/Feed/Feed.stories.tsx
+++ packages/sc-ui/src/components/Feed/Feed.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
 import Feed from './index';
-import {SCFeedTypologyType, Endpoints} from '@selfcommunity/core';
+import {Endpoints} from '@selfcommunity/core';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
