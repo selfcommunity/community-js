0cb94fe060badd24fcf79402e95214d2bb2187b6
mirco.furlan <mirco.furlan@quentral.com>
2022-02-09 13:11:37 +0100

    fix: web-push-messagingfix: web-push-messaging

---
+++ packages/sc-core/src/hooks/useSCWebPushMessaging.ts
@@ -0,0 +1,77 @@
+import {useEffect, useMemo, useState} from 'react';
+import {SCContextType, SCNotificationTopicType, SCTagType, SCUserContextType} from '../types';
+import {useSCContext, useSCUser, WSClientType} from '@selfcommunity/core';
+import WSClient from '../utils/websocket';
+import {SCNotificationMapping, SCNotificationTopics} from '../constants/Notification';
+import {WS_FACILITY_NOTIFY, WS_PROTOCOL_PREFIX, WS_HEARTBEAT_MESSAGE} from '../constants/WebSocket';
+import PubSub from 'pubsub-js';
+
+/**
+ * Custom hook 'useSCWebSocket'
+ * Use this hook to init web socket
+ */
+export default function useSCWebSocket() {
+  const scContext: SCContextType = useSCContext();
+  const scUserContext: SCUserContextType = useSCUser();
+  const [wsInstance, setWsInstance] = useState<WSClientType>(null);
+
+  // Websocket uri and protocols
+  const _wsUri = `wss://${new URL(scContext.settings.portal).hostname}/ws/${WS_FACILITY_NOTIFY}?subscribe-user`;
+  const _wsProtocol =
+    scContext.settings.session.authToken && scContext.settings.session.authToken.accessToken
+      ? `${WS_PROTOCOL_PREFIX}${scContext.settings.session.authToken.accessToken}`
+      : null;
+
+  /**
+   * Check if there is a currently active session and a
+   * wsInstance connection when the provider is mounted for the first time.
+   * If there is an error, it means there is no session.
+   */
+  useEffect(() => {
+    if (scUserContext.user && !wsInstance && _wsUri && _wsProtocol) {
+      setWsInstance(
+        WSClient.getInstance({
+          uri: _wsUri,
+          heartbeatMsg: WS_HEARTBEAT_MESSAGE,
+          protocols: [_wsProtocol],
+          receiveMessage: receiveMessage,
+        })
+      );
+      // Close the socket channel before window unload
+      window.addEventListener('beforeunload', function (event) {
+        wsInstance && wsInstance.close();
+      });
+    }
+    // Disconnect the socket
+    return () => {
+      wsInstance && wsInstance.close();
+    };
+  }, [scContext.settings.session.authToken]);
+
+  /**
+   * Receive a message from wsInstance.
+   */
+  const receiveMessage = (data) => {
+    // receive a message though the websocket from the server
+    let _data = JSON.parse(data);
+    if (_data && _data.type && SCNotificationTopics.includes(_data.type)) {
+      if (_data.type === SCNotificationTopicType.INTERACTION && SCNotificationMapping[_data.data.activity_type]) {
+        setNotificationCounters(_data.data);
+        PubSub.publish(`${_data.type}.${SCNotificationMapping[_data.data.activity_type]}`, _data);
+      } else {
+        PubSub.publish(`${_data.type}`, _data);
+      }
+    }
+  };
+
+  /**
+   * Update user context counters
+   * @param payload
+   */
+  const setNotificationCounters = (payload) => {
+    payload.count_interactions !== undefined && scUserContext.setUnseenInteractionsCounter(payload.count_interactions);
+    payload.count_notification_banners !== undefined && scUserContext.setUnseenInteractionsCounter(payload.count_notification_banners);
+  };
+
+  return {wsInstance, setWsInstance};
+}
