268d04f954a4df21a2f3907504513a7efd7d1bc5
Federico Torresan <federico.torresan@quentral.com>
2022-03-09 10:50:10 +0100

    fix: Fix build process fix: Fix overflow in ReplyComment.tsx

--- packages/sc-ui/src/components/Editor/index.tsx
+++ packages/sc-ui/src/components/Editor/index.tsx
@@ -1,4 +1,6 @@
 import Editor, {EditorProps} from './Editor';
+import RichTextEditor, {TRichTextEditorRef} from './RichTextEditor';
+
 export default Editor;
 import EditorSkeleton from './Skeleton';
-export {EditorProps, EditorSkeleton};
+export {EditorProps, EditorSkeleton, RichTextEditor, TRichTextEditorRef};
