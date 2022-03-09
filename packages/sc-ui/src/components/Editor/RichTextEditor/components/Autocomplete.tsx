4b99c4060a0faf2b35fe6f568e396cab34c37c37
Federico Torresan <federico.torresan@quentral.com>
2022-03-09 10:25:36 +0100

    feat: Import RichTextEditor into ui library

---
+++ packages/sc-ui/src/components/Editor/RichTextEditor/components/Autocomplete.tsx
@@ -0,0 +1,64 @@
+import React, {FunctionComponent} from 'react';
+import {List, ListItem, Paper} from '@mui/material';
+import {styled} from '@mui/material/styles';
+
+export type TAutocompleteItem = {
+  keys: string[];
+  value: any;
+  content: string | JSX.Element;
+};
+
+interface TAutocompleteProps {
+  items: TAutocompleteItem[];
+  top: number;
+  left: number;
+  selectedIndex: number;
+  onClick: (selectedIndex: number) => void;
+}
+
+const PREFIX = 'SCRichTextEditorAutocomplete';
+
+const classes = {
+  container: `${PREFIX}-container`,
+  item: `${PREFIX}-item`
+};
+
+const Root = styled(Paper, {
+  name: PREFIX,
+  slot: 'Root',
+  overridesResolver: (_, styles) => styles.root
+})(() => ({
+  [`&.${classes.container}`]: {
+    minWidth: '200px',
+    position: 'absolute',
+    zIndex: 10
+  },
+  [`& .${classes.item}`]: {
+    cursor: 'pointer'
+  }
+}));
+
+const Autocomplete: FunctionComponent<TAutocompleteProps> = (props) => {
+  if (!props.items.length) {
+    return null;
+  }
+
+  return (
+    <Root
+      className={classes.container}
+      style={{
+        top: props.top,
+        left: props.left
+      }}>
+      <List dense={true}>
+        {props.items.map((item, index) => (
+          <ListItem key={index} className={classes.item} selected={index === props.selectedIndex} onClick={() => props.onClick(index)}>
+            {item.content}
+          </ListItem>
+        ))}
+      </List>
+    </Root>
+  );
+};
+
+export default Autocomplete;
---
+++ packages/sc-ui/src/components/Editor/RichTextEditor/components/Media.tsx
@@ -0,0 +1,95 @@
+import React, {FunctionComponent} from 'react';
+import classNames from 'classnames';
+import {ContentBlock, ContentState} from 'draft-js';
+import {styled} from '@mui/material/styles';
+
+interface MediaProps {
+  block: ContentBlock;
+  contentState: ContentState;
+  blockProps: any;
+  onClick: (block: ContentBlock) => void;
+}
+
+const PREFIX = 'SCRichTextEditorMedia';
+
+const classes = {
+  root: `${PREFIX}-root`,
+  editable: `${PREFIX}-editable`,
+  focused: `${PREFIX}-focused`,
+  centered: `${PREFIX}-centered`,
+  leftAligned: `${PREFIX}-leftAligned`,
+  rightAligned: `${PREFIX}-rightAligned`
+};
+
+const Root = styled('div', {
+  name: PREFIX,
+  slot: 'Root',
+  overridesResolver: (_, styles) => styles.root
+})(({theme}) => ({
+  [`&.${classes.root}`]: {
+    margin: '5px 0 1px',
+    outline: 'none'
+  },
+  [`& .${classes.editable}`]: {
+    cursor: 'pointer',
+    '&:hover': {
+      boxShadow: theme.shadows[3]
+    }
+  },
+  [`& .${classes.focused}`]: {
+    boxShadow: theme.shadows[3]
+  },
+  [`&.${classes.centered}`]: {
+    textAlign: 'center'
+  },
+  [`&.${classes.leftAligned}`]: {
+    textAlign: 'left'
+  },
+  [`&.${classes.rightAligned}`]: {
+    textAlign: 'right'
+  }
+}));
+
+const Media: FunctionComponent<MediaProps> = (props) => {
+  const {url, width, height, alignment, type} = props.contentState.getEntity(props.block.getEntityAt(0)).getData();
+  const {onClick, readOnly, focusKey} = props.blockProps;
+
+  const htmlTag = () => {
+    const componentProps = {
+      src: url,
+      className: classNames(classes.root, {
+        [classes.editable]: !readOnly,
+        [classes.focused]: !readOnly && focusKey === props.block.getKey()
+      }),
+      width: width,
+      height: type === 'video' ? 'auto' : height,
+      onClick: () => {
+        if (readOnly) {
+          return;
+        }
+        onClick(props.block);
+      }
+    };
+
+    if (!type || type === 'image') {
+      return <img {...componentProps} />;
+    }
+    if (type === 'video') {
+      return <video {...componentProps} autoPlay={false} controls />;
+    }
+    return null;
+  };
+
+  return (
+    <Root
+      className={classNames({
+        [classes.centered]: alignment === 'center',
+        [classes.leftAligned]: alignment === 'left',
+        [classes.rightAligned]: alignment === 'right'
+      })}>
+      {htmlTag()}
+    </Root>
+  );
+};
+
+export default Media;
