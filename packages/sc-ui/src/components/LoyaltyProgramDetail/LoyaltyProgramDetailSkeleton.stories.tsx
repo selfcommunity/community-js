15457dc57e0d81fbc4793160baa30d3eb0e78bb0
Simona Melis <simona.melis@quentral.com>
2022-04-06 10:28:33 +0200

    fix: moved loyaltyProgramDetail to sc-ui

--- packages/sc-ui/src/components/LoyaltyProgramDetail/LoyaltyProgramDetailSkeleton.stories.tsx
+++ packages/sc-ui/src/components/LoyaltyProgramDetail/LoyaltyProgramDetailSkeleton.stories.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import {ComponentStory, ComponentMeta} from '@storybook/react';
-
-import LoyaltyProgramDetail from './index';
+import LoyaltyProgramDetailSkeleton from './Skeleton';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Design System/SC TEMPLATES/Loyalty Program Detail',
-  component: LoyaltyProgramDetail
+  title: 'Design System/SC UI/Skeleton/Loyalty Program Detail',
+  component: LoyaltyProgramDetailSkeleton
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-} as ComponentMeta<typeof LoyaltyProgramDetail>;
+} as ComponentMeta<typeof LoyaltyProgramDetailSkeleton>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof LoyaltyProgramDetail> = (args) => <LoyaltyProgramDetail {...args} />;
+const Template: ComponentStory<typeof LoyaltyProgramDetailSkeleton> = (args) => (
+  <div style={{width: 400}}>
+    <LoyaltyProgramDetailSkeleton {...args} />
+  </div>
+);
 
 export const Base = Template.bind({});
 
-Base.args = {
-  /* the args you need here will depend on your component */
-};
+Base.args = {};
