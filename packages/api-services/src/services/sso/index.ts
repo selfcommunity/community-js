4d1869790df8c7511a63864dfb500d3c0abeb323
simonaester@hotmail.it <akitainu93S!>
2022-06-06 17:40:54 +0200

    added sso/comment tests

--- packages/api-services/src/services/sso/index.ts
+++ packages/api-services/src/services/sso/index.ts
@@ -4,8 +4,8 @@ import {SSOSignInType, SSOSignUpType} from '@selfcommunity/types/src/types/sso';
 import {SSOSignUpParams} from '../../types';
 
 export interface SSOApiClientInterface {
-  SignIn(): Promise<SSOSignInType>;
-  SignUp(data: SSOSignUpParams): Promise<SSOSignUpType>;
+  SignIn(token: string): Promise<SSOSignInType>;
+  SignUp(token: string, data: SSOSignUpParams): Promise<SSOSignUpType>;
 }
 /**
  * Contains all the endpoints needed to manage sso.
@@ -14,25 +14,28 @@ export interface SSOApiClientInterface {
 export class SSOApiClient {
   /**
    * This endpoint signs in the user authenticated with the access token.
+   * @param token
    */
-  static SignIn(): Promise<SSOSignInType> {
-    return apiRequest(Endpoints.SignIn.url({}), Endpoints.SignIn.method);
+  static SignIn(token: string): Promise<SSOSignInType> {
+    return apiRequest(Endpoints.SignIn.url({}), Endpoints.SignIn.method, token);
   }
 
   /**
    * This endpoint creates a new account.
-   * @constructor
+   * Only users with the admin role can register new users.
+   * @param token
+   * @param data
    */
-  static SignUp(data: SSOSignUpParams): Promise<SSOSignUpType> {
-    return apiRequest(Endpoints.SignUp.url({}), Endpoints.SignUp.method, data);
+  static SignUp(token: string, data: SSOSignUpParams): Promise<SSOSignUpType> {
+    return apiRequest(Endpoints.SignUp.url({}), Endpoints.SignUp.method, token, data);
   }
 }
 
 export default class SSOService {
-  static async SignIn(): Promise<SSOSignInType> {
-    return SSOApiClient.SignIn();
+  static async SignIn(token: string): Promise<SSOSignInType> {
+    return SSOApiClient.SignIn(token);
   }
-  static async SignUp(data: SSOSignUpParams): Promise<SSOSignUpType> {
-    return SSOApiClient.SignUp(data);
+  static async SignUp(token: string, data: SSOSignUpParams): Promise<SSOSignUpType> {
+    return SSOApiClient.SignUp(token, data);
   }
 }
---
+++ packages/api-services/test/comment-test.js
@@ -0,0 +1,84 @@
+import {generateJWTToken} from '../src/utils/token';
+import {CommentService, FeedObjectService} from '../src/index';
+import {SCFeedObjectTypologyType} from '@selfcommunity/types/src/types';
+import {generateString} from './utils/random';
+import {SCFlagTypeEnum} from '../lib/types/src';
+
+describe('Comment Service Test', () => {
+  let token;
+  let feedObjId;
+  let comment;
+  beforeAll(async () => {
+    token = await generateJWTToken(process.env.SERVICES_USER_ID, process.env.SERVICES_SECRET_KEY);
+  });
+  test('Get all discussions', () => {
+    return FeedObjectService.getAllFeedObjects(SCFeedObjectTypologyType.DISCUSSION).then((data) => {
+      feedObjId = data.results[0].id;
+      expect(data).toBeInstanceOf(Object);
+    });
+  });
+  test('Get all comments', () => {
+    return CommentService.getAllComments({discussion: feedObjId}).then((data) => {
+      expect(data.results).toBeInstanceOf(Array);
+    });
+  });
+  test('Create a comment', () => {
+    const body = {discussion: feedObjId, text: generateString()};
+    return CommentService.createComment(token, body).then((data) => {
+      expect(data.summary).toBe(body.text);
+      comment = data;
+    });
+  });
+  test('Get a specific comment', () => {
+    return CommentService.getASpecificComment(comment.id).then((data) => {
+      expect(data).toBeInstanceOf(Object);
+    });
+  });
+  test('Update a comment', () => {
+    const text = generateString();
+    return CommentService.updateComment(token, comment.id, text).then((data) => {
+      expect(data.summary).toBe(text);
+    });
+  });
+  test('Delete a comment', () => {
+    return CommentService.deleteComment(token, comment.id).then((data) => {
+      expect(data).toBe('');
+    });
+  });
+  test('Restore a comment', () => {
+    return CommentService.restoreComment(token, comment.id).then((data) => {
+      expect(data).toBe('');
+    });
+  });
+  test('Get a specific comment votes list', () => {
+    return CommentService.getSpecificCommentVotesList(comment.id).then((data) => {
+      expect(data).toHaveProperty('count');
+    });
+  });
+  test('Upvote a comment', () => {
+    return CommentService.upvoteComment(token, comment.id).then((data) => {
+      expect(data).toBe('');
+    });
+  });
+  describe('Flag a comment', () => {
+    test('Flag a comment', () => {
+      if (comment.author.id !== 7) {
+        return CommentService.flagComment(token, comment.id, SCFlagTypeEnum.SPAM).then((data) => {
+          expect(data).toBe('');
+        });
+      } else {
+        test.skip;
+      }
+    });
+  });
+  test('Get a specific comment flag list', () => {
+    return CommentService.getSpecificCommentFlags(token, comment.id).then((data) => {
+      expect(data).toHaveProperty('count');
+    });
+  });
+  test('Get a specific comment flag status', () => {
+    return CommentService.getSpecificCommentFlagStatus(token, comment.id).then((data) => {
+      expect(data).toBeInstanceOf(Object);
+    });
+  });
+});
