643ff780293c480ca2721f282f9e00fccb870086
simonaester@hotmail.it <akitainu93S!>
2022-06-22 13:07:32 +0200

    fix: fixed data portability test

--- packages/api-services/test/dataPortability-test.js
+++ packages/api-services/test/dataPortability-test.js
@@ -1,19 +1,27 @@
 import {DataPortabilityService} from '../src/index';
 
 describe('Data Portability Service Test', () => {
-  test('Generate data portability', () => {
-    return DataPortabilityService.generateDataPortability().then((data) => {
-      expect(data).toBeInstanceOf(Object);
-    });
-  });
-  test('Download data portability', () => {
-    return DataPortabilityService.downloadDataPortability().then((data) => {
-      expect(data).toBeInstanceOf(Object);
-    });
-  });
+  let dataPortability;
   test('Get data portability status', () => {
     return DataPortabilityService.dataPortabilityStatus().then((data) => {
+      dataPortability = data;
       expect(data).toBeInstanceOf(Object);
     });
+  });
+  test('Generate data portability', () => {
+    if (!dataPortability.requested_at) {
+      return DataPortabilityService.generateDataPortability().then((data) => {
+        expect(data).toBeInstanceOf(Object);
+      });
+    } else {
+      test.skip;
+    }
+  });
+  test('Download data portability', () => {
+    if (!dataPortability.requested_at && !dataPortability.computing) {
+      return DataPortabilityService.downloadDataPortability().then((data) => {
+        expect(data).toBeInstanceOf(Object);
+      });
+    }
   });
 });
