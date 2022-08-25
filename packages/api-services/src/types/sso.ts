6ab656e3e6a16cb6dbb6060ce8ae0cd497bbb87b
Federico Torresan <federico.torresan@quentral.com>
2022-08-25 16:58:45 +0200

    fix: Fix SSO SignUp params

--- packages/api-services/src/types/sso.ts
+++ packages/api-services/src/types/sso.ts
@@ -3,10 +3,10 @@
  */
 
 export interface SSOSignUpParams {
-  ext_id: number;
+  ext_id?: number;
   username: string;
-  email?: string;
-  password: string;
+  email: string;
+  password?: string;
   role?: string | null;
   tags?: number[] | null;
 }
