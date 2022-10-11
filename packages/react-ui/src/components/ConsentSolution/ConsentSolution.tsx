549617b65a2696899f373263aae42f22fdd59334
Mirco Furlan <mirk8xr@gmail.com>
2022-10-11 11:34:05 +0200

    fix: ConsentSolution components

--- packages/react-ui/src/components/ConsentSolution/ConsentSolution.tsx
+++ packages/react-ui/src/components/ConsentSolution/ConsentSolution.tsx
@@ -366,7 +366,7 @@ export default function ConsentSolution(inProps: ConsentSolutionProps): JSX.Elem
         const url = window.URL.createObjectURL(new Blob([res], {type: 'application/zip'}));
         const link = document.createElement('a');
         link.href = url;
-        link.setAttribute('download', `udp_${scUserContext.user.username}_${}.zip`);
+        link.setAttribute('download', `udp_${scUserContext.user.username}.zip`);
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
