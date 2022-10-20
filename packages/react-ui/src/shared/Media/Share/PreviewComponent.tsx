d2b937475f3d00b0734d912eda82d03ec9791616
Mirco Furlan <mirk8xr@gmail.com>
2022-10-20 14:47:00 +0200

    fix: Media preview and comment votes

--- packages/react-ui/src/shared/Media/Share/PreviewComponent.tsx
+++ packages/react-ui/src/shared/Media/Share/PreviewComponent.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import {styled} from '@mui/material/styles';
-import LazyLoad from 'react-lazyload';
-import Box from '@mui/material/Box';
-import FeedObject from '../../../components/FeedObject';
-import {SCFeedObjectTemplateType} from '../../../types/feedObject';
-import {MAX_PRELOAD_OFFSET_VIEWPORT} from '../../../constants/LazyLoad';
-import FeedObjectSkeleton from '../../../components/FeedObject/Skeleton';
+import React from "react";
+import { styled } from "@mui/material/styles";
+import LazyLoad from "react-lazyload";
+import Box from "@mui/material/Box";
+import FeedObject from "../../../components/FeedObject";
+import { SCFeedObjectTemplateType } from "../../../types/feedObject";
+import { MAX_PRELOAD_OFFSET_VIEWPORT } from "../../../constants/LazyLoad";
+import FeedObjectSkeleton from "../../../components/FeedObject/Skeleton";
+import { CacheStrategies } from "@selfcommunity/utils";
 
 const PREFIX = 'SCPreviewMediaShare';
 
@@ -33,29 +34,16 @@ export default ({medias = [], adornment = null}: {medias: any[]; GridImageProps?
       {medias.length > 0 && (
         <Root>
           {adornment}
-          {medias.map((media) => (
-            <LazyLoad
-              height={400}
-              key={media.id}
-              placeholder={
-                <FeedObjectSkeleton
-                  template={SCFeedObjectTemplateType.SNIPPET}
-                  elevation={0}
-                  variant={'outlined'}
-                  className={classes.sharePlaceholder}
-                />
-              }
-              once
-              offset={MAX_PRELOAD_OFFSET_VIEWPORT}>
-              <Box className={classes.sharePreview}>
+          {medias.map((media, i) => (
+              <Box className={classes.sharePreview} key={i}>
                 <FeedObject
                   feedObjectId={media.embed.metadata.id}
                   feedObjectType={media.embed.metadata.type}
                   variant={'outlined'}
                   template={SCFeedObjectTemplateType.SHARE}
+                  cacheStrategy={CacheStrategies.CACHE_FIRST}
                 />
               </Box>
-            </LazyLoad>
           ))}
         </Root>
       )}
