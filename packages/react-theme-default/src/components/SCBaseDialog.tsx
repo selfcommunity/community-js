d6c8484b2e510858125eaab8388ea8434b0dc69e
Federico Torresan <federico.torresan@quentral.com>
2023-01-23 17:48:20 +0100

    BaseDialog theme

--- packages/react-theme-default/src/components/SCBaseDialog.tsx
+++ packages/react-theme-default/src/components/SCBaseDialog.tsx
@@ -1,8 +1,38 @@
 const Component = {
   styleOverrides: {
     root: ({theme}: any) => ({
-      '& .infinite-scroll-component > .': {
-        marginBottom: theme.spacing(2.5)
+      '& h2': {
+        fontSize: '1.286rem',
+        fontWeight: theme.typography.fontWeightBold,
+        padding: theme.spacing(3),
+        '& .MuiIconButton-root': {
+          right: theme.spacing(3),
+          top: theme.spacing(3)
+        }
+      },
+      '& .MuiDialogContent-root': {
+        paddingLeft: 0,
+        paddingRight: 0,
+        marginLeft: theme.spacing(3),
+        marginRight: theme.spacing(3),
+        '& .infinite-scroll-component': {
+          ' > .': {
+            marginBottom: theme.spacing(2.5)
+          },
+          '& .MuiList-root': {
+            paddingTop: 0,
+            paddingBottom: 0,
+            '& .MuiListItem-root': {
+              paddingLeft: 0,
+              '& .SCBaseItemButton-root': {
+                borderRadius: 0
+              }
+            }
+          }
+        },
+      },
+      '& .MuiDialogActions-spacing': {
+        padding: theme.spacing(2, 3)
       }
     })
   }
