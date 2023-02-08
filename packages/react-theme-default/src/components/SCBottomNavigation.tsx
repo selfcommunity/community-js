9694009a5a89c8cd1374e74b78d5bfd45cde48a8
Federico Torresan <federico.torresan@quentral.com>
2023-02-08 16:48:37 +0100

    fix theme

--- packages/react-theme-default/src/components/SCBottomNavigation.tsx
+++ packages/react-theme-default/src/components/SCBottomNavigation.tsx
@@ -3,6 +3,7 @@ const Component = {
     root: ({theme}: any) => ({
       '& .SCBottomNavigation-action': {
         color: theme.palette.primary.main,
+        borderTop: `1px solid transparent`,
         '&.Mui-selected, &:hover': {
           color: theme.palette.secondary.main,
           borderTop: `1px solid ${theme.palette.secondary.main}`
