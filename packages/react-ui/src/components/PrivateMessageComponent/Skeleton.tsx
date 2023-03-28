6f8dd6899cfb6b56691481d742f04fc45238fb66
Simona Melis <simonaester@hotmail.it>
2023-03-28 16:17:40 +0200

    fix: fixed pm component skeleton on mobile viewport

--- packages/react-ui/src/components/PrivateMessageComponent/Skeleton.tsx
+++ packages/react-ui/src/components/PrivateMessageComponent/Skeleton.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {styled} from '@mui/material/styles';
-import {Grid} from '@mui/material';
+import {Grid, useMediaQuery, useTheme} from '@mui/material';
 import PrivateMessageSnippetsSkeleton from '../PrivateMessageSnippets/Skeleton';
 import PrivateMessageThreadSkeleton from '../PrivateMessageThread/Skeleton';
 import classNames from 'classnames';
+import {SCThemeType} from '@selfcommunity/react-core';
 
 const PREFIX = 'SCPrivateMessageComponentSkeleton';
 
@@ -50,11 +51,15 @@ export type PrivateMessageComponentProps = React.PropsWithChildren<PrivateMessag
  */
 export default function PrivateMessageComponentSkeleton(props: PrivateMessageComponentProps): JSX.Element {
   const {className} = props;
+  const theme = useTheme<SCThemeType>();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
   return (
     <Root container className={classNames(classes.root, className)}>
-      <Grid item xs={12} md={5} className={classes.snippetsSection}>
-        <PrivateMessageSnippetsSkeleton />
-      </Grid>
+      {!isMobile && (
+        <Grid item xs={12} md={5} className={classes.snippetsSection}>
+          <PrivateMessageSnippetsSkeleton />
+        </Grid>
+      )}
       <Grid item xs={12} md={7} className={classes.threadSection}>
         <PrivateMessageThreadSkeleton />
       </Grid>
