31564a47becfddba80fd516a47fbe8b3e1ae60ac
Federico Torresan <federico.torresan@quentral.com>
2023-05-08 14:34:12 +0200

    fix: Fix error on request abort

--- packages/api-services/src/utils/apiRequest.ts
+++ packages/api-services/src/utils/apiRequest.ts
@@ -1,22 +1,24 @@
 import client from '../client';
 import {AxiosRequestConfig} from 'axios';
+import {SCOPE_API_SERVICES} from '../constants/Errors';
+import {Logger} from '@selfcommunity/utils';
 
 export function apiRequest(config: AxiosRequestConfig) {
   return client
     .request(config)
     .then((res: any) => {
       if (!res) {
-        console.log(`Request ${config.method} ${config.url} aborted.`);
+        Logger.info(SCOPE_API_SERVICES, `Request ${config.method} ${config.url} aborted.`);
         return Promise.reject(res);
       }
       if (res.status >= 300) {
-        console.log(`Unable to ${config.method} ${config.url} (Response code: ${res.status}).`);
+        Logger.info(SCOPE_API_SERVICES, `Unable to ${config.method} ${config.url} (Response code: ${res.status}).`);
         return Promise.reject(res);
       }
       return Promise.resolve(res.data);
     })
     .catch((error) => {
-      console.log(`Unable to ${config.method} ${config.url} ${error}`);
+      Logger.warn(SCOPE_API_SERVICES, `Unable to ${config.method} ${config.url} ${error}`);
       return Promise.reject(error);
     });
 }
