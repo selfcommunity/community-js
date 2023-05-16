7f500c403389800f4b27197af7f79c794f373495
Simona Melis <simonaester@hotmail.it>
2023-05-16 12:22:15 +0200

    fix: fixed new message autocomplete

--- packages/react-theme-default/src/components/SCPrivateMessageThread.tsx
+++ packages/react-theme-default/src/components/SCPrivateMessageThread.tsx
@@ -86,11 +86,10 @@ const Component = {
             marginLeft: theme.spacing(1)
           },
           '& .SCPrivateMessageThread-autocomplete': {
-            minWidth: theme.spacing(27),
+            width: '100%',
             marginLeft: theme.spacing(1),
             maxHeight: theme.mixins.toolbar.minHeight,
             overflow: 'auto',
-            scrollbarWidth: 'none',
             '& .MuiAutocomplete-tag': {
               height: theme.spacing(3),
               '& .MuiChip-deleteIcon': {
