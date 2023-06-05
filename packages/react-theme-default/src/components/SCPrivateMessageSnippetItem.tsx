637cffb78276203de6a0f3dd7c764812379d8140
Simona Melis <simonaester@hotmail.it>
2023-06-05 15:29:16 +0200

    fix: fixed new message icon position, translations. Asana issues: -https://app.asana.com/0/1202638443591994/1204723795300006/f -https://app.asana.com/0/1202638443591994/1204723795300028/f - https://app.asana.com/0/1202638443591994/1204723795300020/f -https://app.asana.com/0/1202638443591994/1204721434773099/f

--- packages/react-theme-default/src/components/SCPrivateMessageSnippetItem.tsx
+++ packages/react-theme-default/src/components/SCPrivateMessageSnippetItem.tsx
@@ -2,27 +2,33 @@ const Component = {
   styleOverrides: {
     root: ({theme}: any) => ({
       borderRadius: theme.spacing(1.5),
-      padding: `${theme.spacing(0)}!important`,
       boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
       marginBottom: theme.spacing(1),
       '& .SCPrivateMessageSnippetItem-time': {
         float: 'right',
         fontSize: theme.typography.fontWeightRegular
       },
+      '& .MuiListItemButton-root': {
+        [theme.breakpoints.up('sm')]: {
+          '&.SCPrivateMessageSnippetItem-unread': {paddingRight: theme.spacing(7)},
+          paddingRight: theme.spacing(4)
+        },
+        [theme.breakpoints.down('sm')]: {
+          '&.SCPrivateMessageSnippetItem-unread': {paddingRight: theme.spacing(3)},
+          paddingRight: theme.spacing(2)
+        }
+      },
       '& .MuiListItemText-primary': {
         '& .SCPrivateMessageSnippetItem-username': {
           fontWeight: theme.typography.fontWeightBold
         },
         '& .SCPrivateMessageSnippetItem-badge-label': {
-          marginLeft: theme.spacing(1),
+          marginLeft: theme.spacing(0.5),
           borderRadius: 0,
           fontSize: '0.5rem'
         }
       },
       '& .MuiListItemText-secondary': {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
         '& .MuiTypography-root': {
           display: 'inline-block',
           width: '80%',
@@ -32,6 +38,8 @@ const Component = {
         }
       },
       '& .MuiListItemSecondaryAction-root': {
+        display: 'flex',
+        alignItems: 'center',
         position: 'absolute',
         top: theme.spacing(3),
         right: theme.spacing(0.5),
