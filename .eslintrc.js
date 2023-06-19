92ee19f54b8b488f9c0930ebd2e6c775b5a6e4f3
Mirco Furlan <mirk8xr@gmail.com>
2023-06-19 18:03:20 +0200

    fix: typescript ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,9 @@ const config = {
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-floating-promises": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
     "react-hooks/exhaustive-deps": "off"
   },
   "settings": {
