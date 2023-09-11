ff9021b50a6b41b804b1c6d9c025aefdb1f12719
Mirco Furlan <mirk8xr@gmail.com>
2023-09-11 17:39:30 +0200

    fix: build optimization

--- packages/react-theme-default/tsup.config.ts
+++ packages/react-theme-default/tsup.config.ts
@@ -10,22 +10,22 @@ export const baseConfig: Options = {
   skipNodeModulesBundle: true,
   watch: false,
   // entry: ['src/index.ts', 'src/components/*.*'],
-	entry: ['src/index.ts'],
+  entry: ['src/index.ts'],
   shims: false
 };
 
 export default defineConfig([
-	{
-		...baseConfig,
-		outDir: 'lib/cjs',
-		format: ['cjs'],
-		tsconfig: './tsconfig-cjs.build.json',
-	},
-	{
-		...baseConfig,
-		outDir: 'lib/esm',
-		format: ['esm'],
-		target: 'es2020',
-		tsconfig: './tsconfig-esm.build.json',
-	},
+  {
+    ...baseConfig,
+    outDir: 'lib/cjs',
+    format: ['cjs'],
+    tsconfig: './tsconfig-cjs.build.json'
+  },
+  {
+    ...baseConfig,
+    outDir: 'lib/esm',
+    format: ['esm'],
+    target: 'es2020',
+    tsconfig: './tsconfig-esm.build.json'
+  }
 ]);
