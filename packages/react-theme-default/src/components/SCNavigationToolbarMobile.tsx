4abcf9be18143f6d5312768442d27b356394ec71
fedetorre <fede.torre@gmail.com>
2024-01-29 15:16:37 +0100

    fix: Fix margin on logo mobile

--- packages/react-theme-default/src/components/SCNavigationToolbarMobile.tsx
+++ packages/react-theme-default/src/components/SCNavigationToolbarMobile.tsx
@@ -3,7 +3,7 @@ const Component = {
     root: ({theme}: any) => ({
       padding: theme.spacing(0, 1, 0, 1),
       '& .SCNavigationToolbarMobile-logo': {
-        margin: theme.spacing(0.5, 2, 0.5, 0),
+        margin: theme.spacing(0.5, 2, 0.5, 2),
         flexGrow: 1,
         '& img': {
           verticalAlign: 'middle',
