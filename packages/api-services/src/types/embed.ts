1eda00aa9d1bc38c36637aac8763d9e46bf914ce
Simona Melis <simona.melis@quentral.com>
2024-05-27 11:07:28 +0200

    perf: removed vimeo

--- packages/api-services/src/types/embed.ts
+++ packages/api-services/src/types/embed.ts
@@ -12,7 +12,7 @@ export interface EmbedUpdateParams {
 
   /**
    * Metadata associated to this embed. It mus be a valid json object.
-   * For embed_type sc_vimeo or sc_link it contains metadata associated
+   * For embed_type sc_link it contains metadata associated
    * with the external resource. For embed_type sc_shared_object it contains a field
    * type that is the type of the object (Discussion or Post) and a field id
    * that contains the id of the object shared
