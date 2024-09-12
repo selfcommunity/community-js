f4d7a35de587ef3dddb12b3d0d0f0e5cbc2ddeb8
Mirco Furlan <mirk8xr@gmail.com>
2024-09-12 17:45:43 +0200

    fix: bugfix Info details event

--- packages/react-theme-default/src/components/SCSuggestedEventsWidget.tsx
+++ packages/react-theme-default/src/components/SCSuggestedEventsWidget.tsx
@@ -35,7 +35,7 @@ const Component = {
 
                     '& p': {
                       marginTop: 1,
-                      fontSize: '0.74rem'
+                      fontSize: '0.70rem'
                     }
                   }
                 }
