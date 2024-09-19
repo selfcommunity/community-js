1f0364fa2a85516202123cbfea86b0cc5461b5fb
Simona Melis <simona.melis@quentral.com>
2024-09-19 15:40:24 +0200

    fix: relative time formatter util

--- packages/react-ui/src/utils/formatRelativeTime.ts
+++ packages/react-ui/src/utils/formatRelativeTime.ts
@@ -2,7 +2,7 @@ export const getRelativeTime = (date) => {
   const diffInMilliseconds = new Date().getTime() - new Date(date).getTime();
   const seconds = Math.floor(diffInMilliseconds / 1000);
   const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
+  const hours = Math.ceil(minutes / 60);
   const days = Math.floor(hours / 24);
   const weeks = Math.floor(days / 7);
   const months = Math.floor(days / 30);
