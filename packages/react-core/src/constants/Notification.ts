b3df7359d334426f09c6619630f1ade4a8ca462a
Mirco Furlan <mirk8xr@gmail.com>
2024-11-21 15:52:02 +0100

    fix: livestream

--- packages/react-core/src/constants/Notification.ts
+++ packages/react-core/src/constants/Notification.ts
@@ -23,7 +23,7 @@ export const SCNotificationMapping = {
   16: SCNotificationTypologyType.FOLLOW,
   37: SCNotificationTypologyType.PRIVATE_MESSAGE,
   105: SCNotificationTypologyType.DELETE_PRIVATE_MESSAGE,
-  106: SCNotificationTypologyType.LIVE_STREAM_STARTED,
+  26: SCNotificationTypologyType.LIVE_STREAM_STARTED,
   50: SCNotificationTypologyType.DELETED_FOR_ADVERTISING,
   51: SCNotificationTypologyType.DELETED_FOR_AGGRESSIVE,
   52: SCNotificationTypologyType.DELETED_FOR_VULGAR,
