19534d55131093f9c9967951636f260b6fde93c9
Mirco Furlan <mirk8xr@gmail.com>
2024-12-30 17:54:28 +0100

    fix: load video (problem with lazy load in the pwa preview container)

--- packages/react-templates/src/components/CategoryFeed/CategoryFeed.stories.tsx
+++ packages/react-templates/src/components/CategoryFeed/CategoryFeed.stories.tsx
@@ -9,7 +9,7 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 export const Base: StoryObj<typeof CategoryFeedTemplate> = {
   args: {
-    categoryId: 13
+    categoryId: 2
   },
   render: (args) => (
     <div style={{maxWidth: '1200px', width: '100%', height: '500px'}}>
@@ -17,9 +17,9 @@ export const Base: StoryObj<typeof CategoryFeedTemplate> = {
     </div>)
 };
 
-export const BaseContainerFixed: StoryObj<typeof CategoryTemplate> = {
+export const BaseContainerFixed: StoryObj<typeof CategoryFeedTemplate> = {
 	args: {
-		categoryId: 1
+		categoryId: 2
 	},
 	render: (args) => {
 		return <div style={{position: 'fixed', bottom: 0, left: 0, right: 0, top: 70, zIndex: 1, maxWidth: '100% !important', height: '92vh', overflow: 'auto'}} id="scrollableDiv">
