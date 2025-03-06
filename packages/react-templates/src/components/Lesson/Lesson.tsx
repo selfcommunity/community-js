47920c9108ce1ae54f083031ccf47af98ca4a2c9
Simona Melis <simona.melis@quentral.com>
2025-03-06 12:16:06 +0100

    fix: fixes

--- packages/react-templates/src/components/Lesson/Lesson.tsx
+++ packages/react-templates/src/components/Lesson/Lesson.tsx
@@ -155,7 +155,7 @@ export default function Lesson(inProps: LessonProps): JSX.Element {
   const [completed, setCompleted] = useState<boolean>(scLesson?.completion_status === SCCourseLessonCompletionStatusType.COMPLETED);
   const currentData = useMemo(() => {
     if (!scCourse || !scLesson) return null;
-    return getCurrentSectionAndLessonIndex(scCourse, scLesson.section_id, scLesson.id);
+    return getCurrentSectionAndLessonIndex(scCourse, _sectionId, _lessonId);
   }, [scCourse, scLesson]);
   const [currentSectionIndex, setCurrentSectionIndex] = useState(currentData?.currentSectionIndex || 0);
   const [currentLessonIndex, setCurrentLessonIndex] = useState(currentData?.currentLessonIndex || 0);
