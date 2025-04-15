72216c51f526773011fc3c46565679f9bbb84e20
Mirco Furlan <mirk8xr@gmail.com>
2025-04-15 11:50:39 +0200

    fix: test course enabled

--- packages/react-ui/src/components/CreateCourseButton/CreateCourseButton.tsx
+++ packages/react-ui/src/components/CreateCourseButton/CreateCourseButton.tsx
@@ -78,7 +78,7 @@ export default function CreateCourseButton(inProps: CreateCourseButtonProps): JS
     () =>
       preferences &&
       features &&
-      features.includes(SCFeatureName.TAGGING) &&
+      features.includes(SCFeatureName.COURSE) &&
       SCPreferences.CONFIGURATIONS_COURSES_ENABLED in preferences &&
       preferences[SCPreferences.CONFIGURATIONS_COURSES_ENABLED].value,
     [preferences, features]
