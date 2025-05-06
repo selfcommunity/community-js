12c24769a44a3ab373e07abf0f81f3a7585fcc73
Mirco Furlan <mirk8xr@gmail.com>
2025-05-06 12:07:31 +0200

    fix: paymentOrders

--- packages/react-theme-default/src/components/SCPaymentOrders.tsx
+++ packages/react-theme-default/src/components/SCPaymentOrders.tsx
@@ -3,19 +3,37 @@ const Component = {
     root: ({theme}: any) => ({
       '& .SCPaymentOrders-content': {
         position: 'relative',
-        padding: '30px 10px',
+        margin: theme.spacing(1, 2),
         '& table': {
           '& tr': {
             '& th': {
               zIndex: 1
             }
+          },
+          '& .SCGroup-root, .SCCategory-root': {
+            borderRadius: theme.shape.borderRadius,
+            maxWidth: '96%'
+          },
+          '& .SCPaymentProduct-root': {
+            borderRadius: `${theme.shape.borderRadius}px !important`,
+            border: '1px solid #e5e5e5',
+            boxShadow: 'none',
+            color: theme.palette.text.primary,
+            '& .MuiAccordionSummary-root': {
+              backgroundColor: theme.palette.background.paper,
+              opacity: 1,
+              borderRadius: theme.shape.borderRadius
+            }
           }
+        },
+        '& .MuiCircularProgress-root': {
+          display: 'block',
+          margin: `${theme.spacing(7)} auto`
         }
       },
       '& .SCPaymentOrders-filters': {
         alignItems: 'center',
-        marginTop: theme.spacing(),
-        marginBottom: theme.spacing(2),
+        margin: theme.spacing(2),
         '& .SCPaymentOrders-search': {
           '& .MuiInputBase-root': {
             paddingRight: 0,
