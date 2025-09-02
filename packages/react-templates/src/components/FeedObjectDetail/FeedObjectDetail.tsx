87ab5fc31d8cb2c2b56f3a8609aa810d88fbd913
Domenico Garruzzo <domenico.garruzzo@quentral.com>
2025-09-02 16:47:09 +0200

    fix: fixed how to show comments. Linear: https://linear.app/quentral/issue/QUE-1905/frontend-pur-avendo-disabilitato-i-commenti-appare-sulla-destra-un

--- packages/react-templates/src/components/FeedObjectDetail/FeedObjectDetail.tsx
+++ packages/react-templates/src/components/FeedObjectDetail/FeedObjectDetail.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import {useMemo, useState} from 'react';
 import {Box, Grid, Hidden, styled} from '@mui/material';
 import {
   CommentsFeedObject,
@@ -143,8 +143,12 @@ export default function FeedObjectDetail(inProps: FeedObjectDetailProps): JSX.El
   const scPreferences: SCPreferencesContextType = useSCPreferences();
 
   // RETRIVE OBJECTS
-  const {obj, setObj, error} = useSCFetchFeedObject({id: feedObjectId, feedObject, feedObjectType});
+  const {obj, error} = useSCFetchFeedObject({id: feedObjectId, feedObject, feedObjectType});
   const [comments, setComments] = useState<SCCommentType[]>([]);
+
+  const commentsEnabled =
+    SCPreferences.CONFIGURATIONS_COMMENTS_ENABLED in scPreferences.preferences &&
+    scPreferences.preferences[SCPreferences.CONFIGURATIONS_COMMENTS_ENABLED].value;
 
   /**
    * Compute preferences
@@ -205,7 +209,9 @@ export default function FeedObjectDetail(inProps: FeedObjectDetailProps): JSX.El
         <Grid item xs={12} md={7}>
           <FeedObject {...FeedObjectProps} feedObject={obj} template={SCFeedObjectTemplateType.DETAIL} onReply={handleReply} />
           {renderAdvertising()}
-          <CommentsFeedObject key={`comments_${obj.id}`} showTitle feedObject={obj} comments={comments} {...CommentsFeedObjectProps} />
+          {commentsEnabled && (
+            <CommentsFeedObject key={`comments_${obj.id}`} showTitle feedObject={obj} comments={comments} {...CommentsFeedObjectProps} />
+          )}
         </Grid>
         <Grid item xs={12} md={5}>
           <Hidden mdDown>
