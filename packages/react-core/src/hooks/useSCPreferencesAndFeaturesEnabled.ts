16759ea7a84512de074d2e1ca56d3eba2a1271f9
Mirco Furlan <mirk8xr@gmail.com>
2025-09-18 16:49:19 +0200

    fix: hooks useSCPreference, useSCPreferenceEnabled, useSCPreferencesEnabled, useSCFeatureEnabled, useSCFeaturesEnabled, useSCPreferencesAndFeaturesEnabled,

---
+++ packages/react-core/src/hooks/useSCPreferencesAndFeaturesEnabled.ts
@@ -0,0 +1,38 @@
+import {useMemo} from 'react';
+import {SCFeatureName} from '@selfcommunity/types';
+import {useSCPreferences} from '@selfcommunity/react-core';
+
+/**
+ * Custom hook preferences and features at the same time
+ * @param preferences - array of preference keys
+ * @param features - array of feature keys
+ * @returns boolean - true only if all preferences and features are enabled
+ *
+ * Ex.
+ * const isEnabled = useSCPreferencesAndFeaturesEnabled(
+ *     [
+ *       SCPreferences.CONFIGURATIONS_POST_USER_ADDRESSING_ENABLED,
+ *       SCPreferences.CONFIGURATIONS_SCHEDULED_POSTS_ENABLED
+ *     ],
+ *     [SCFeatureName.TAGGING]
+ *   );
+ */
+export default function useSCPreferencesAndFeaturesEnabled(preferences: string[], features: SCFeatureName[] = []): boolean {
+  const {preferences: preferencesContext, features: featuresContext} = useSCPreferences();
+
+  return useMemo(() => {
+    // Check available context
+    if (!preferencesContext || !featuresContext) {
+      return false;
+    }
+
+    // Check every preferences
+    const preferencesEnabled = preferences.every((key) => key in preferencesContext && preferencesContext[key].value);
+
+    // Check every features
+    const featuresEnabled = features.every((feature) => featuresContext.includes(feature));
+
+    // Return true only if all preferences and features are enabled
+    return preferencesEnabled && featuresEnabled;
+  }, [preferencesContext, featuresContext, preferences, features]);
+}
